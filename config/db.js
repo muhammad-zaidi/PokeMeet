8aa90dbe8aa7e05e88db15f2ca9458ee16211746
Muhammad Zaidi <smazaidi2003@gmail.com>
2020-07-15 21:21:56 -0400

    Fixed deprecation (useCreateIndex)

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
-mongoose.set('useCreateIndex', true);
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     }); //Connect to the database; await as mongoose.connect returns a promise
     console.log('MongoDB Connected');
   } catch (err) {
