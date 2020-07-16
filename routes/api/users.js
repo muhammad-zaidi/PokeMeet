738ae19c0cad5cf0b97795e289fd3757295d0360
Muhammad Zaidi <smazaidi2003@gmail.com>
2020-07-15 21:44:18 -0400

    User finished

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
 const { validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 const User = require('../../models/User');
 
@@ -29,14 +31,40 @@ router.post(
 
     try {
       //See if user exists; send back error if it already does
+      let user = await User.findOne({
+        email,
+      });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
 
       //Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
 
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
       //Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      //Save user to database
+      await user.save();
 
       //Return jsonwebtoken
 
-      res.send('User route');
+      res.send('User registered');
     } catch (err) {
       console.error(err.message);
       res.status(500).send('server error');
