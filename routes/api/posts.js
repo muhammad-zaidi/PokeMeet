355ac10a3005ea2d6eec91afc6b853aa1cac01ad
Muhammad Zaidi <smazaidi2003@gmail.com>
2020-07-28 17:34:50 -0400

    Liking a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -72,7 +72,7 @@ router.get('/:id', auth, async (req, res) => {
   }
 });
 
-// @route    Delete api/posts/:id
+// @route    DELETE api/posts/:id
 // @desc     Delete a post from the database
 // @access   Private
 router.delete('/:id', auth, async (req, res) => {
@@ -94,6 +94,30 @@ router.delete('/:id', auth, async (req, res) => {
     if (err.kind == 'ObjectId') {
       return res.status(404).json({ msg: 'Post not found' });
     }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    PUT api/posts/like/:id
+// @desc     Like a post
+// @access   Private
+
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if post is already liked by that user
+    if (
+      post.likes.filter((like) => like.user.toString() == req.user.id).length >
+      0
+    ) {
+      res.status(400).json({ msg: 'Post already liked' });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
     res.status(500).send('Server Error');
   }
 });
